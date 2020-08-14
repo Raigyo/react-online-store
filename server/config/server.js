0b25583bb2939bebce569349ac4ec0ea11397eb7
chilot <https://github.com/Raigyo>
2020-08-14 14:35:24 +0200

    remove some files

--- server/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '79d35add72c6360e37f99844232c67a2'),
-    },
-  }
-});
