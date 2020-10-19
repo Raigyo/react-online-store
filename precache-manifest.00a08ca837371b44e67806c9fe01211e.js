f678e4c490a288943171e146022adac7839f2f15
chilot <https://github.com/Raigyo>
2020-10-19 16:41:08 +0200

    Updates

---
+++ precache-manifest.00a08ca837371b44e67806c9fe01211e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805043865939c8b16860167d871eecb4",
+    "url": "/react-online-store/index.html"
+  },
+  {
+    "revision": "0c0f5e0a4edd8e490ede",
+    "url": "/react-online-store/static/css/2.465d1ef4.chunk.css"
+  },
+  {
+    "revision": "e8c412997898ba1e15ca",
+    "url": "/react-online-store/static/css/main.2c2ded1c.chunk.css"
+  },
+  {
+    "revision": "0c0f5e0a4edd8e490ede",
+    "url": "/react-online-store/static/js/2.e4b2b82a.chunk.js"
+  },
+  {
+    "revision": "0870fbf2c23685da2f05883650f7c87a",
+    "url": "/react-online-store/static/js/2.e4b2b82a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c412997898ba1e15ca",
+    "url": "/react-online-store/static/js/main.94603927.chunk.js"
+  },
+  {
+    "revision": "00e813d0a71e4b952fd3",
+    "url": "/react-online-store/static/js/runtime-main.ca7a4fbe.js"
+  },
+  {
+    "revision": "c00393282f51980d787c97600d381a5b",
+    "url": "/react-online-store/static/media/logo.c0039328.svg"
+  }
+]);
